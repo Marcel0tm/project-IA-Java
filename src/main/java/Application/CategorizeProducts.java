da389d7cf0ac0e39a6d42a29af25d9d8e1a55bcb
Talisson Marcelo <marcelosilvatmls@gmail.com>
2024-03-06 21:50:28 -0300

    Added user input and some restrictions for outputs

--- src/main/java/Application/CategorizeProducts.java
+++ src/main/java/Application/CategorizeProducts.java
@@ -5,15 +5,44 @@ import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CategorizeProducts {
     public static void main(String[] args) {
-        var user = "Escova de cabelo";
-        var system = "Você é um categorizador de produtos";
 
+        var sc = new Scanner(System.in);
+
+        System.out.print("Digite as categorias válidas de produtos: ");
+        var categorias = sc.nextLine();
+        System.out.print("Digite o nome do produto: ");
+        var user = sc.nextLine();
+
+        var system = """
+        Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+        
+        Escolha uma categoria dentra a lista abaixo:
+        
+        %s
+        
+        Exemplo de uso:
+        
+        Pergunta: Bola de Futebol
+        Resposta: Esportes
+        
+        Regras a serem seguidas:
+        
+        Caso o usuário pergunte algo que não seja sobre categorização de produto, você deve responder que não pode ajudar com isso pois seu papel é apenas categorizar os produtos.
+        Caso o usuário informe um produto que não se encaixe em nenhuma das categorias passadas você deve dizer que este produto pertence a categoria de "Outros"
+        """.formatted(categorias);
+
+        requests(user, system);
+    }
+
+    public static void requests(String user, String system) {
         var key = System.getenv("KEY_OPENAI");
-        var service = new OpenAiService(key);
+        var service = new OpenAiService(key, Duration.ofSeconds(30));
 
         var completionRequest = ChatCompletionRequest
                 .builder()
@@ -22,15 +51,11 @@ public class CategorizeProducts {
                         new ChatMessage(ChatMessageRole.USER.value(), user),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
-                .n(5)
                 .build();
 
         service
                 .createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(c -> {
-                    System.out.println(c.getMessage().getContent());
-                    System.out.println("----------------------------");
-                });
+                .forEach(c -> System.out.println(c.getMessage().getContent()));
     }
 }
